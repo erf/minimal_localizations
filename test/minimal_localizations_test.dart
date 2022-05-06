615e28832ec2691e7746e161387e3bf15c217203
Erlend Lind Madsen <erlendf80@gmail.com>
2022-05-06 12:27:32 +0200

    test: add widget tests

--- test/minimal_localizations_test.dart
+++ test/minimal_localizations_test.dart
@@ -1,10 +1,50 @@
-import 'dart:ui';
-
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:minimal_localizations/minimal_localizations.dart';
 
+final minimalLocalizationsDelegate = MinimalLocalizationsDelegate(
+  {
+    'en': {'title': 'Localizations'},
+    'nb-NO': {'title': 'Lokaliseringer'},
+  },
+);
+
+Widget buildTestWidgetWithLocale(Locale locale) {
+  return MaterialApp(
+    locale: locale,
+    localizationsDelegates: [
+      ...GlobalMaterialLocalizations.delegates,
+      minimalLocalizationsDelegate,
+    ],
+    supportedLocales: minimalLocalizationsDelegate.supportedLocales(),
+    home: Scaffold(
+      body: Builder(
+        builder: (context) => Text(
+          MinimalLocalizations.of(context).string('title'),
+        ),
+      ),
+    ),
+  );
+}
+
 void main() {
+  testWidgets('MyTestApp find [en] text', (WidgetTester tester) async {
+    await tester.pumpWidget(buildTestWidgetWithLocale(const Locale('en')));
+    await tester.pump();
+    final hiFinder = find.text('Localizations');
+    expect(hiFinder, findsOneWidget);
+  });
+
+  testWidgets('MyTestApp find [nb-NO] text', (WidgetTester tester) async {
+    await tester
+        .pumpWidget(buildTestWidgetWithLocale(const Locale('nb', 'NO')));
+    await tester.pump();
+    final hiFinder = find.text('Lokaliseringer');
+    expect(hiFinder, findsOneWidget);
+  });
+
   test('Test MinimalLocalizationsDelegate is supported', () {
     final delegate = MinimalLocalizationsDelegate({
       'en': {'title': 'Minimal Localizations'},
